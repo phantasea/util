463acd3f34a25f25eef840f38f84e95046db4ba6
phantasea <phantasea@gmail.com>
2023-11-12 13:01:58 +0200

    bins/epubimage.py improved

--- bins/epubimage.py
+++ bins/epubimage.py
@@ -4,21 +4,22 @@
 from ebooklib import epub
 import os
 
-#set up the epub file
 book = epub.EpubBook()
 book.set_title("my_comic_book")
 book.set_language('en')
 
 content = [u'<html> <head></head> <body>']
+curdir = os.path.abspath(os.path.dirname("."))
+imgsrc = '<img src="' + curdir + '/{}"/>'
 
-for filename in os.listdir("images"):
+for filename in os.listdir(curdir):
   if filename.endswith(".jpg"):
-    image_file = open("images/" + filename, 'rb').read()
+    image_file = open(curdir + "/" + filename, 'rb').read()
     image = epub.EpubImage()
-    image.file_name = "images/" + filename
+    image.file_name = curdir[1:] + "/" + filename
     image.content = image_file
     book.add_item(image)
-    content.append('<img src="images/{}"/>'.format(filename))
+    content.append(imgsrc.format(filename))
 
 content.append('</body> </html>')
 c1 = epub.EpubHtml(title='Images', file_name='images.xhtml', lang='en')
@@ -27,5 +28,4 @@ c1.content=''.join(content)
 book.add_item(c1)
 book.spine = ['nav', c1]
 
-#write epub to file
 epub.write_epub("my_comic.epub", book, {})
