c71f12dbeef71953580c183a7f52586bdf1a5d1f
phantasea <phantasea@gmail.com>
2026-07-26 15:02:58 +0300

    audconv/imgconv changed

--- batch_audconv.sh
+++ batch_audconv.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-[[ $# != 1 ]]   && exit 1
-[[ ! -d "$1" ]] && exit 1
+dir=""
+if [ $# == 1 ]; then
+    [[ ! -d "$1" ]] && exit 1
+    dir="$1"
+elif [ $# == 2 ]; then
+    [[ ! -d "$2" ]] && exit 1
+    dir="$2"
+else
+    exit 1
+fi
 
-abspath=$(realpath "$1")
+abspath=$(realpath $dir)
 cd "$abspath"
+
+rate=96
+while getopts "r:" opt; do
+    case $opt in
+        r)
+           rate="$OPTARG"
+           ;;
+
+        ?) echo 'Usage: batch_audconv [-r rate] <file>'
+           exit 1;;
+    esac
+done
 
 for filename in ./*.mp3; do
     echo $filename
-    audconv "$filename"
+    audconv -r${rate} "$filename"
     sleep 25s
 done
