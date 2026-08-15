a756e19b97796f2474163bf66ae2981e283c10db
phantasea <phantasea@gmail.com>
2026-08-15 22:20:17 +0300

    mymount/imgconv update

--- batch_imgconv.sh
+++ batch_imgconv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ratio=${1:-60}
-for filename in ./* ; do
-    mogrify -resize ${ratio}% "$filename"
-    sleep 5s
+for file in ./* ; do
+    #mogrify -resize ${ratio:-80}% "$file"
+    pio "$file" --quality ${ratio:-80} --output $file.webp > /dev/null 2>&1
+    sleep 3s
 done
